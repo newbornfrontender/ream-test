f1850ddcfbac6172dd154535ae2460e8aac2ca9f
Infant Frontender <newbornfrontender@gmail.com>
2018-10-12 19:30:16 +0300

    Добавил структуру

--- pages/Index.vue
+++ pages/Index.vue
@@ -1,9 +1,25 @@
 <script>
   export default {
-    name: 'PageIndex',
+    name: 'Index',
   };
 </script>
 
 <template>
-  <h1>Home</h1>
+  <section>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>Home</h1>
+    </main>
+  </section>
 </template>
