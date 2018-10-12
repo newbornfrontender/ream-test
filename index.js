f583495e99ab15e8aed2d279848b060e9873ea53
Infant Frontender <newbornfrontender@gmail.com>
2018-10-12 18:44:38 +0300

    Создал основной файл

---
+++ index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default () => {
+  const router = new Router({
+    mode: 'history',
+    routes: [{
+      path: '/',
+      component: () => import('./pages/Index.vue'),
+    }],
+  });
+
+  return {
+    router,
+  };
+};
