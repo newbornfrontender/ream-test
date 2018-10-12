5c12c683cb341f379d88315725da4764410f5dbb
Infant Frontender <newbornfrontender@gmail.com>
2018-10-12 19:30:29 +0300

    Добавил новые страницы

---
+++ pages/About.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+    name: 'About',
+  };
+</script>
+
+<template>
+  <section>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>About</h1>
+    </main>
+  </section>
+</template>
