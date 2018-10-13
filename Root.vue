3ce4802362b2a5a30e20db111aa9432e94d86a5a
Infant Frontender <newbornfrontender@gmail.com>
2018-10-13 21:12:39 +0300

    Создал Root компонент

---
+++ Root.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    name: 'Root',
+  };
+</script>
+
+<template>
+  <section>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+
+          <li>
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <router-view />
+    </main>
+  </section>
+</template>
