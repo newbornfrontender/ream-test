9e712cd10c32cdce1e476b40d5145fc2778107d2
Infant Frontender <newbornfrontender@gmail.com>
2018-10-13 22:04:06 +0300

    Добавил компоненты списков

---
+++ components/ItemsList.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    props: {
+      tag: {
+        type: String,
+        required: true,
+        validator: value => {
+          const ol = value === 'ol';
+          const ul = value === 'ul';
+
+          return ol || ul;
+        },
+      },
+
+      items: {
+        type: Array,
+        required: true,
+      },
+    },
+
+    render (createElement) {
+      return createElement(this.tag, this.items.map(function (item) {
+        return createElement('li', item.name);
+      }));
+    },
+  };
+</script>
